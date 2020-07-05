6639a83e0e2806b2012866a8df07651f8b29838a
Tanuka1999 <60867568+Tanuka1999@users.noreply.github.com>
2020-07-05 20:05:02 +0530

    Add files via upload

---
+++ Speakhello.js
@@ -0,0 +1,8 @@
+﻿(function (window) {
+    var helloSpeaker = new Object();
+    var speakWord = "hello";
+    helloSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
